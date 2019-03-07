8ffac0985f5d493e3645c054ed02b8306d3347ff
John Lowe <jblowe@berkeley.edu>
2019-03-07 13:58:21 -0800

    HMP-53: production deployment should not perform db:migrate

--- relink.sh
+++ relink.sh
@@ -28,7 +28,7 @@ fi
 RUN_DIR=$1/portal
 if [ ! -d ${RUN_DIR} ] ; then echo "$1 does not exist... exiting" ; exit 1 ; fi
 if [ -d search_$2 -a ! -L search_$2 ] ; then echo "search_$2 exists and is not a symlink ... cowardly refusal to rm it and relink it" ; exit 1 ; fi
-rm -f search_$2
+rm search_$2
 ln -s ${RUN_DIR} search_$2
 if [ "$3" == "production" ]; then
   echo "remaking links to db and log for production deployment"
@@ -39,12 +39,10 @@ if [ "$3" == "production" ]; then
   # link the db directory to the "permanent" db directory
   rm -rf db/
   ln -s /var/blacklight-db/$2 db
-  export RAILS_ENV=production
-  rake db:migrate
+  # no database migration is done it this case; assumes existing logs and dbs will work...
 else
   echo "leaving db and log as is for dev deployment"
   cd ${RUN_DIR}
-  # nb: right now, only the production migration works, for some reason...
   export RAILS_ENV=development
   rake db:migrate
 fi
