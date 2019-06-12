565cd714ac15439064dc6412b916ff34c2a2fe43
John Lowe <jblowe@berkeley.edu>
2019-06-12 09:44:45 -0700

    CSW-68: revised Blacklight components for 'demo' portals

---
+++ extras/application_helper.rb
@@ -0,0 +1,147 @@
+module ApplicationHelper
+
+  def render_csid csid, derivative
+      "https://webapps.cspace.berkeley.edu/#TENANT#/imageserver/blobs/#{csid}/derivatives/#{derivative}/content"
+  end
+
+  def render_status options={}
+      options[:value].collect do |status|
+        content_tag(:span, status, style: 'color: red;')
+      end.join(', ').html_safe
+  end
+
+  def render_media options={}
+    # return a list of cards or images
+    content_tag(:div) do
+      options[:value].collect do |blob_csid|
+         content_tag(:a, content_tag(:img, '',
+           src: render_csid(blob_csid, 'Medium'),
+           class: 'thumbclass'),
+           href: "https://webapps.cspace.berkeley.edu/#TENANT#/imageserver/blobs/#{blob_csid}/content",
+           target: 'original',
+           style: 'padding: 3px;',
+           class: 'hrefclass')
+      end.join.html_safe
+    end
+  end
+
+  # use imageserver and blob csid to serve audio
+  def render_audio_csid options={}
+    # render audio player
+    content_tag(:div) do
+      options[:value].collect do |audio_csid|
+        content_tag(:audio,
+          content_tag(:source, "I'm sorry; your browser doesn't support HTML5 audio in MPEG format.",
+             src: "https://webapps.cspace.berkeley.edu/#TENANT#/imageserver/blobs/#{audio_csid}/content",
+             id: 'audio_csid',
+             type: 'audio/mpeg'),
+             controls: 'controls',
+             style: 'height: 60px; width: 640px;')
+      end.join.html_safe
+    end
+  end
+
+  # use imageserver and blob csid to serve video
+  def render_video_csid options={}
+    # render video player
+    content_tag(:div) do
+      options[:value].collect do |video_csid|
+        content_tag(:video,
+          content_tag(:source, "I'm sorry; your browser doesn't support HTML5 video in MP4 with H.264.",
+             src: "https://webapps.cspace.berkeley.edu/#TENANT#/imageserver/blobs/#{video_csid}/content",
+             id: 'video_csid',
+             type: 'video/mp4'),
+             controls: 'controls',
+             style: 'width: 640px;')
+      end.join.html_safe
+    end
+  end
+
+  # serve audio directy via apache (apache needs to be configured to serve nuxeo repo)
+  def render_audio_directly options={}
+    # render audio player
+    content_tag(:div) do
+      options[:value].collect do |audio_md5|
+        l1 = audio_md5[0..1]
+        l2 = audio_md5[2..3]
+        content_tag(:audio,
+          content_tag(:source, "I'm sorry; your browser doesn't support HTML5 audio in MPEG format.",
+             src: "https://cspace-prod-02.ist.berkeley.edu/#TENANT#_nuxeo/data/#{l1}/#{l2}/#{audio_md5}",
+             id: 'audio_md5',
+             type: 'audio/mpeg'),
+             controls: 'controls',
+             style: 'height: 60px; width: 640px;')
+      end.join.html_safe
+    end
+  end
+
+  # serve audio directy via apache (apache needs to be configured to serve nuxeo repo)
+  def render_video_directly options={}
+    # render video player
+    content_tag(:div) do
+      options[:value].collect do |video_md5|
+        l1 = video_md5[0..1]
+        l2 = video_md5[2..3]
+        content_tag(:video,
+          content_tag(:source, "I'm sorry; your browser doesn't support HTML5 video in MP4 with H.264.",
+             src: "https://cspace-prod-02.ist.berkeley.edu/#TENANT#_nuxeo/data/#{l1}/#{l2}/#{video_md5}",
+             id: 'video_md5',
+             type: 'video/mp4'),
+             controls: 'controls',
+             style: 'width: 640px;')
+      end.join.html_safe
+    end
+  end
+
+
+  def render_x3d_csid options={}
+    # render x3d object
+    content_tag(:div) do
+      options[:value].collect do |x3d_csid|
+        content_tag(:x3d,
+          content_tag(:scene,
+              content_tag(:inline, '',
+                 url: "https://webapps.cspace.berkeley.edu/#TENANT#/imageserver/blobs/#{x3d_csid}/content",
+                 id: 'x3d',
+                 type: 'model/x3d+xml')),
+        style: 'margin-bottom: 6px; height: 660px; width: 660px;')
+      end.join.html_safe
+    end
+  end
+
+  # serve X3D directy via apache (apache needs to be configured to serve nuxeo repo)
+  def render_x3d_directly options={}
+    # render x3d player
+    content_tag(:div) do
+      options[:value].collect do |x3d_md5|
+        l1 = x3d_md5[0..1]
+        l2 = x3d_md5[2..3]
+        content_tag(:x3d,
+          content_tag(:scene,
+              content_tag(:inline, '',
+                 url: "https://cspace-prod-02.ist.berkeley.edu/#TENANT#_nuxeo/data/#{l1}/#{l2}/#{x3d_md5}",
+                 class: 'x3d',
+                 type: 'model/x3d+xml')),
+        style: 'margin-bottom: 6px; height: 660px; width: 660px;')
+      end.join.html_safe
+    end
+  end
+
+  # compute ark from museum number and render as a link
+  def render_ark options={}
+    # encode museum number as ARK ID, e.g. 11-4461.1 -> hm21114461@1, K-3711a-f -> hm210K3711a%2Df
+    options[:value].collect do |musno|
+      ark = 'hm2' + if musno.include? '-'
+        left, right = musno.split('-', 2)
+        left = '1' + left.rjust(2, '0')
+        right = right.rjust(7, '0')
+        CGI.escape(left + right).sub('.','@').sub('-','=')
+      else
+        'x' + CGI.escape(musno).sub('.','@')
+      end
+
+      link_to "ark:/21549/" + ark, "https://n2t.net/ark:/21549/" + ark
+    end.join.html_safe
+  end
+
+end
---
+++ install_ucb.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#set -e
+#set -x
+tenant=$1
+portal_config_file=$2
+current_directory=`pwd`
+extra_dir="../extras"
+
+# check the command line parameters
+
+if [ ! -f "${portal_config_file}" ]; then
+  echo "Can't find portal config file '${portal_config_file}'. Please verify name and location"
+  echo "$0 tenant portal_config_file"
+  exit
+fi
+
+cd portal
+
+if [ ! -d "${extra_dir}" ]; then
+  echo "Can't find directory '${extra_dir}'. Please verify name and location"
+  echo "$0 tenant portal_config_file"
+  exit
+fi
+
+# ensure that the code is fresh
+check checkout -- ${extra_dir}/*
+perl -i -pe "s/#TENANT#/${tenant}/g" ${extra_dir}/*
+
+# configure BL using existing Portal config file
+python3 ${extra_dir}/ucb_bl.py ${portal_config_file} > bl_config_temp.txt
+cat ${extra_dir}/catalog_controller.template bl_config_temp.txt > app/controllers/catalog_controller.rb
+rm bl_config_temp.txt
+
+# nb: the header logos for all ucb tenants are already in the public static directory
+#     we just need to copy the one for this tenant to the right place.
+cp public/header-logo-${tenant}.png public/header-logo.png
+
+# use the generic header, footer, etc. partials
+cp ${extra_dir}/_header_navbar.html.erb app/views/shared/
+cp ${extra_dir}/_footer.html.erb app/views/shared/
+cp ${extra_dir}/_splash.html.erb app/views/shared/
+cp ${extra_dir}/_home_text.html.erb app/views/catalog/
+cp ${extra_dir}/_search_form.html.erb app/views/catalog/
+
+# we may someday add customized versions of these partials, but not yet
+#cp ${extra_dir}/_user_util_links.html.erb app/views/
+#cp ${extra_dir}/${tenant}_catalog_controller.rb app/controllers/catalog_controller.rb
+#cp ${extra_dir}/${tenant}_header_navbar.html.erb app/views/shared/_header_navbar.html.erb
+#cp ${extra_dir}/${tenant}_footer.html.erb app/views/shared/_footer.html.erb
+#cp ${extra_dir}/${tenant}_home_text.html.erb app/views/catalog/_home_text.html.erb
+#cp ${extra_dir}/${tenant}_search_form.html.erb app/views/catalog/_search_form.html.erb
+#cp ${extra_dir}/${tenant}_splash.html.erb app/views/shared/_splash.html.erb
+
+# generic helpers, but they do need to be configured per-tenant
+cp ${extra_dir}/application_helper.rb app/helpers
+cp ${extra_dir}/catalog_helper_behavior.rb app/helpers/blacklight
+cp ${extra_dir}/blacklight.yml config
