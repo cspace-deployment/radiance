9a01c9788ca3101e0b60c0b00d1765df0ec158fd
John Lowe <jblowe@berkeley.edu>
2019-06-13 07:59:53 -0700

    CSW-68: helpers scripts for BL demo installs

---
+++ extras/configall.sh
@@ -0,0 +1,5 @@
+cd ~/projects/bampfa    ; ./install_ucb.sh bampfa    ~/projects/django_example_config/bampfa/config/public_collection_info.csv
+cd ~/projects/botgarden ; ./install_ucb.sh botgarden ~/projects/django_example_config/botgarden/config/plantinfo.csv
+cd ~/projects/cinefiles ; ./install_ucb.sh cinefiles ~/projects/django_example_config/cinefiles/config/public_collection_info.csv
+#cd ~/projects/pahma     ; ./install_ucb.sh pahma     ~/projects/django_example_config/pahma/config/pahmapublicparms.csv
+cd ~/projects/ucjeps    ; ./install_ucb.sh ucjeps    ~/projects/django_example_config/ucjeps/config/ucjepspublicparms.csv
