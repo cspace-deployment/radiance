e6500f26ca89d702f2ef3d5f1d4ceb9d106efac9
John Lowe <jblowe@berkeley.edu>
2020-03-13 08:51:02 -0700

    CSW-274: incremental improvements to cinefiles blacklight look-and-feel and functionality

--- install_ucb.sh
+++ install_ucb.sh
@@ -22,28 +22,28 @@ if [ ! -d "${extra_dir}" ]; then
   exit
 fi
 
-# ensure that the code is fresh
-git checkout -- ${extra_dir}/*
 perl -i -pe "s/#TENANT#/${tenant}/g" ${extra_dir}/*
 
-# configure BL using existing Portal config file
+# configure generic tenant BL controller using existing Portal config file
 python3 ${extra_dir}/ucb_bl.py ${portal_config_file} > bl_config_temp.txt
 cat ${extra_dir}/catalog_controller.template bl_config_temp.txt > app/controllers/catalog_controller.rb
 rm bl_config_temp.txt
+
+# now apply customizations, if any
 
 # nb: the header logos for all ucb tenants are already in the public static directory
 #     we just need to copy the one for this tenant to the right place.
 cp public/header-logo-${tenant}.png public/header-logo.png
 cp ${extra_dir}/cspace_fav.png app/assets/images/favicon.png
 
-# use the generic header, footer, etc. partials
+# use our generic header, footer, etc. partials
 cp ${extra_dir}/_header_navbar.html.erb app/views/shared/
 cp ${extra_dir}/_footer.html.erb app/views/shared/
 cp ${extra_dir}/_splash.html.erb app/views/shared/
 cp ${extra_dir}/_home_text.html.erb app/views/catalog/
 cp ${extra_dir}/_search_form.html.erb app/views/catalog/
 
-# we may someday add customized versions of these partials, but not yet
+# possible customized partials
 #cp ${extra_dir}/_user_util_links.html.erb app/views/
 cp ${extra_dir}/${tenant}_catalog_controller.rb app/controllers/catalog_controller.rb
 cp ${extra_dir}/${tenant}_header_navbar.html.erb app/views/shared/_header_navbar.html.erb
@@ -51,6 +51,10 @@ cp ${extra_dir}/${tenant}_footer.html.erb app/views/shared/_footer.html.erb
 #cp ${extra_dir}/${tenant}_home_text.html.erb app/views/catalog/_home_text.html.erb
 #cp ${extra_dir}/${tenant}_search_form.html.erb app/views/catalog/_search_form.html.erb
 cp ${extra_dir}/${tenant}_splash.html.erb app/views/shared/_splash.html.erb
+cp ${extra_dir}/${tenant}_extras.scss app/assets/stylesheets/extras.scss
+
+# custom signup for cinefiles
+cp ${extra_dir}/${tenant}_new.html.erb app/views/devise/registrations/new.html.erb
 
 # generic helpers and config, but they do need to be configured per-tenant
 cp ${extra_dir}/application_helper.rb app/helpers
