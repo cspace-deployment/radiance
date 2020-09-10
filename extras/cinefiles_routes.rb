a85e4a508d435b8fbaed8a52fa544a35abc0f548
mcampos-quinn <mcq@berkeley.edu>
2020-09-09 22:23:07 -0700

    add handling for custom error pages (404/500); add CineFiles customized error pages

---
+++ extras/cinefiles_routes.rb
@@ -0,0 +1,37 @@
+Rails.application.routes.draw do
+
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+  concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
+  mount Blacklight::Engine => '/'
+  mount BlacklightAdvancedSearch::Engine => '/'
+
+  Blacklight::Marc.add_routes(self)
+  root to: "catalog#index"
+  concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+    concerns :range_searchable
+
+  end
+  devise_for :users
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  get '/*ark_tag/:naan/:ark' => 'catalog#decode_ark', constraints: { ark_tag:'ark:' }
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
