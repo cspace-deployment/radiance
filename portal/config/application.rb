5684aaa65b6b6a01c8126f52eef13fe8f80e7324
mcampos-quinn <mcq@berkeley.edu>
2020-09-14 23:49:17 -0700

    HMP-160: make generic error pages (404/500) that rely on any UCB TENANT's existing default page layout, without presupposing any other customizations (like images, etc.); NOTE: this deletes the default Rails /public/404.html and /public/500.html

--- portal/config/application.rb
+++ portal/config/application.rb
@@ -15,5 +15,8 @@ module Portal
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # this points app exceptions to the routes specified in config/routes.rb
+    config.exceptions_app = self.routes
   end
 end
