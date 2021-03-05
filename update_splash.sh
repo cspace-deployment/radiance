0e060fa287a51886f8c117a907325280a28a068f
John Lowe <jblowe@berkeley.edu>
2021-03-05 15:03:34 -0800

    HMP-173: initial version of script to update blacklight splash pages

---
+++ update_splash.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo
+    echo "    Usage: $0 museum"
+    echo
+    echo "    e.g.   $0 pahma"
+    echo
+    echo "    (only works on RTL Ubuntu servers!)"
+    exit
+fi
+
+TENANT=$1
+
+cd ~/projects/radiance/
+
+# tidy up repo
+git clean -fd
+git reset --hard
+
+git checkout master
+git pull -v
+
+# copy the needed files
+cp extras/${TENANT}_splash.html.erb ~/projects/search_${TENANT}/app/views/shared/_splash.html.erb 
+cp portal/public/*.jpg ~/projects/search_${TENANT}/public/
+cp portal/public/*.png ~/projects/search_${TENANT}/public/
+
+# restart blacklight for ${TENANT}
+touch ~/projects/search_${TENANT}/tmp/restart.txt
