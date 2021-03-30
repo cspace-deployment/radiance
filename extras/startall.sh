4f5332ae06e7b3c8504514de5be8c3a99c5c0cbd
John Lowe <jblowe@berkeley.edu>
2021-03-29 19:23:51 -0700

    HMP-142: initial refactoring of ucb blacklight deploy apparatus: remove more cruft

--- extras/startall.sh
+++
@@ -1,5 +0,0 @@
-cd ~/projects/pahma/portal/ ; passenger start -p 3000 -d
-cd ~/projects/ucjeps/portal/ ; passenger start -p 3001 -d
-cd ~/projects/botgarden/portal/ ; passenger start -p 3002 -d
-cd ~/projects/bampfa/portal/ ; passenger start -p 3003 -d
-cd ~/projects/cinefiles/portal/ ; passenger start -p 3004 -d
