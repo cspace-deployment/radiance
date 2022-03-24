29f3fd2aa27dafc3e26660ad5a6e84e42fe3f763
John Lowe <jblowe@berkeley.edu>
2022-03-23 18:41:14 -0700

    HMP-226: initial revison of dbcleanup.sh -- root privs not required, etc.

--- dbcleanup.sh
+++ dbcleanup.sh
@@ -1,66 +1,62 @@
 #!/bin/bash
 
+NOTIFY="jblowe@berkeley.edu"
+
 echo "CLEANUP - $(/bin/date) - cleanup starting" 1>&2
-
-if ! /bin/systemctl --quiet is-active apache2.service
-then
-   echo "CLEANUP - $(/bin/date) - apache is not running!" 1>&2
-   echo "CLEANUP - $(/bin/date) - finished" 1>&2
-   exit
-fi
-
-echo "CLEANUP - $(/bin/date) - stopping apache" 1>&2
-while /bin/systemctl --quiet is-active apache2.service
-do
-   /bin/systemctl stop apache2.service
-   /bin/sleep 5
-done
 
 for TENANT in pahma cinefiles
 do
-   SQLERRORS=1
+   DBDIR="/var/cspace/${TENANT}/blacklight/db"
+   DBNAME="production.sqlite3.${TENANT}.$(/bin/date +%Y%m%d)"
+   SQLERRORS=0
 
-   echo "CLEANUP - $TENANT - $(/bin/date) - gzipping current db" 1>&2
-   if [ -f /root/dbbackup/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d)".gz ] || ! /bin/gzip -c /var/cspace/${TENANT}/blacklight/db/production.sqlite3 > /root/dbbackup/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d)".gz
+   echo "CLEANUP - $TENANT - $(/bin/date) - dumping current db" 1>&2
+   if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 ".dump" > ${DBDIR}/${DBNAME}
+   then
+      ((SQLERRORS++))
+   fi
+
+   echo "CLEANUP - $TENANT - $(/bin/date) - gzipping dump" 1>&2
+   if [ -f ${DBDIR}/${DBNAME}.gz ] || ! /bin/gzip ${DBDIR}/${DBNAME}
    then
       echo "CLEANUP - $TENANT - $(/bin/date) - gzip failed, aborting" 1>&2
-      /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up failed" felder@berkeley.edu <<< 'Unable to gzip, see /root/dbclean.log for details or make sure there is not already a backup for the current date.'
+      /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up failed" ${NOTIFY} <<< 'Unable to gzip, see dbclean.log'
    else
       echo "CLEANUP - $TENANT - $(/bin/date) - copying to drive" 1>&2
-      if ! /usr/bin/rclone copy --ignore-existing /root/dbbackup/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d)".gz blacklight_db_backups:/blacklight_db_backups/prod/${TENANT}/
+      if ! /usr/bin/rclone copy --ignore-existing ${DBDIR}/${DBNAME}.gz blacklight_db_backups:/blacklight_db_backups/prod/${TENANT}/
       then
          echo "CLEANUP - $TENANT - $(/bin/date) - rclone failed, aborting" 1>&2
-         /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up failed" felder@berkeley.edu <<< 'Unable to copy to drive, see /root/dbclean.log for details.'
+         /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up failed" ${NOTIFY} <<< 'Unable to copy to drive, see dbclean.log for details.'
       else
          SQLERRORS=0
 
          echo "CLEANUP - $TENANT - $(/bin/date) - deleting searches" 1>&2
-         if ! /usr/bin/sqlite3 /var/cspace/${TENANT}/blacklight/db/production.sqlite3 "delete from searches;"
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from searches;"
          then
             ((SQLERRORS++))
          fi
 
          echo "CLEANUP - $TENANT - $(/bin/date) - deleting users" 1>&2
-         if ! /usr/bin/sqlite3 /var/cspace/${TENANT}/blacklight/db/production.sqlite3 "delete from users where email like 'guest_%';"
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from users where email like 'guest_%';"
          then
             ((SQLERRORS++))
          fi
 
          echo "CLEANUP - $TENANT - $(/bin/date) - deleting bookmarks" 1>&2
-         if ! /usr/bin/sqlite3 /var/cspace/${TENANT}/blacklight/db/production.sqlite3 "delete from bookmarks where user_id not in (select id from users);"
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from bookmarks where user_id not in (select id from users);"
          then
             ((SQLERRORS++))
          fi
 
          echo "CLEANUP - $TENANT - $(/bin/date) - vacuuming" 1>&2
-         if ! /usr/bin/sqlite3 /var/cspace/${TENANT}/blacklight/db/production.sqlite3 "vacuum;"
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "vacuum;"
          then
             ((SQLERRORS++))
          fi
 
          if [ "$SQLERRORS" -gt "0" ]
          then
-            /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up completed with errors" felder@berkeley.edu <<< 'See /root/dbclean.log for details.'
+            /usr/bin/mail -s "CLEANUP - $TENANT Blacklight DB clean up completed with errors" ${NOTIFY} <<< 'See dbclean.log for details.'
          fi
       fi
    fi
@@ -68,15 +64,8 @@ do
    if [ "$SQLERRORS" -eq "0" ]
    then
       echo "CLEANUP - $TENANT - $(/bin/date) - removing 4 week old backup" 1>&2
-      /bin/rm -f /root/dbbackup/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d --date='4 weeks ago')".gz
+      /bin/rm -f ${DBDIR}/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d --date='4 weeks ago')".gz
    fi
-done
-
-echo "CLEANUP - $(/bin/date) - starting apache" 1>&2
-while ! /bin/systemctl --quiet is-active apache2.service
-do
-   /bin/systemctl start apache2.service
-   /bin/sleep 5
 done
 
 echo "CLEANUP - $(/bin/date) - finished" 1>&2
