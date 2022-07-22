2f145286f7ef55005d13d2b1cb6c9140f2ed7ec1
John Lowe <jblowe@berkeley.edu>
2022-07-22 16:29:37 -0700

    HMP-428: add 'permitted classes' to YAML parsing in ActiveRecord

--- extras/application.rb
+++ extras/application.rb
@@ -11,6 +11,15 @@ module Portal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.active_record.yaml_column_permitted_classes = [
+      ActiveSupport::HashWithIndifferentAccess,
+      ActiveSupport::TimeWithZone,
+      ActiveSupport::TimeZone,
+      Date,
+      Symbol,
+      Time
+    ]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
