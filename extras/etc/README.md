91cea252a87f25bd23b56e26d62c512c09ff32b8
John Lowe <jblowe@berkeley.edu>
2022-08-12 15:53:50 -0700

    HMP-243: clean out extras/ directory

--- extras/etc/README.md
+++
@@ -1,6 +0,0 @@
-
-```
-python pick8.py 4
-for t in botgarden bampfa cinefiles pahma ucjeps; do python makestatic.py ${t}.static.csv > ${t}_splash.html.erb ; done
-```
-
--- extras/etc/bampfa-custom/make-bampfa.sh
+++
@@ -1,9 +0,0 @@
-# configure BL using existing Portal config file
-python3 extras/ucb_bl.py $1 > bl_config_temp.txt
-cat extras/catalog_controller.template bl_config_temp.txt > bampfa_catalog_controller.rb
-rm bl_config_temp.txt
-
-# to make a new splash partial for a tenant.
-# e.g. pick out 15 images to include in 4 x 4 splash partial
-python3 extras/etc/pick8.py 4
-python3 extras/etc/makestatic.py bampfa.static.csv > bampfa_splash.html.erb
