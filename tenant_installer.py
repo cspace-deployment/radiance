f1183b480e4a2a3823758380fb4c34099d618072
mcampos-quinn <mcq@berkeley.edu>
2022-09-06 16:57:35 -0700

    HMP-243: updates to tenant_installer.py to be more agnostic about extras/common/ dir contents

--- tenant_installer.py
+++ tenant_installer.py
@@ -11,38 +11,39 @@ if len(sys.argv) <=1 or sys.argv[1] not in tenant_list:
     sys.exit(1)
 else:
     tenant = sys.argv[1]
-os.chdir('extras/'+tenant)
+app_path = pathlib.Path('portal/').resolve()
+
+# first copy over common files
+os.chdir('extras/common')
+common_dir = pathlib.Path('.')
+common_files = sorted(common_dir.glob('**/*.*'))
+# print(common_files)
+for file_path in common_files:
+    # print(file_path)
+    common_file_resolved = file_path.resolve()
+    print(common_file_resolved)
+    dest_file = app_path.joinpath(file_path).resolve()
+    print(dest_file)
+    shutil.copyfile(common_file_resolved,dest_file)
+
+    tmp = str(dest_file)+".tmp"
+    tmp_lines = []
+    placeholder = "#TENANT#"
+    with open(dest_file,'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if placeholder in line:
+                line = line.replace(placeholder,tenant)
+            tmp_lines.append(line)
+    with open(tmp,'w') as f:
+        for line in tmp_lines:
+            f.write(line)
+
+    tmp = pathlib.Path(tmp).resolve()
+    shutil.copyfile(tmp,dest_file)
+    tmp.unlink()
 
 # copy over tenant files
-tenant_path = pathlib.Path('.').resolve()
-app_path = pathlib.Path('../../portal').resolve()
-
+tenant_path = pathlib.Path('../'+tenant)
+os.chdir(tenant_path)
 shutil.copytree(tenant_path,app_path,dirs_exist_ok=True)
-
-# copy over common files
-common_files = [
-    "app/helpers/application_helper.rb",
-    "app/presenters/blacklight/thumbnail_presenter.rb"
-    ]
-common_dir = pathlib.Path('../common')
-for file_path in common_files:
-    test = app_path.joinpath(file_path).resolve()
-    print(common_dir.joinpath(file_path))
-    if not test.exists():
-        shutil.copytree(common_dir.joinpath(file_path).parent.resolve(),test.parent.resolve(), dirs_exist_ok=True)
-        tmp = str(test)+".tmp"
-        tmp_lines = []
-        placeholder = "#TENANT#"
-        with open(test,'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                if placeholder in line:
-                    line = line.replace(placeholder,tenant)
-                tmp_lines.append(line)
-        with open(tmp,'w') as f:
-            for line in tmp_lines:
-                f.write(line)
-        tmp = pathlib.Path(tmp).resolve()
-        test = pathlib.Path(test).resolve()
-        shutil.copy(tmp,file_path)
-        tmp.unlink()
