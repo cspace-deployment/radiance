caea440d6f1e3f162d9471e81e61f429bc22d0d9
John Lowe <jblowe@berkeley.edu>
2023-06-26 09:50:58 -0700

    CSW-877: revert dbcleanup-aws.sh to sql-based cleanup; rake tasks are no better

--- dbcleanup-aws.sh
+++ dbcleanup-aws.sh
@@ -10,12 +10,12 @@ for TENANT in pahma cinefiles bampfa
 do
    DBDIR="/cspace/blacklight/${TENANT}/db"
    DBNAME="production.sqlite3.${TENANT}.$(/bin/date +%Y%m%d)"
-   ERRORS=0
+   SQLERRORS=0
 
    echo "CLEANUP - $TENANT - $(/bin/date) - dumping current db" 1>&2
    if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 ".dump" > ${DBDIR}/${DBNAME}
    then
-      ((ERRORS++))
+      ((SQLERRORS++))
    fi
 
    echo "CLEANUP - $TENANT - $(/bin/date) - gzipping dump" 1>&2
@@ -25,47 +25,45 @@ do
       /usr/bin/mail -r "cspace-support@lists.berkeley.edu" -s "CLEANUP - $BL_ENVIRONMENT $TENANT Blacklight DB clean up failed" ${NOTIFY} <<< 'Unable to gzip, see dbclean.log'
    else
       echo "CLEANUP - $TENANT - $(/bin/date) - copying to s3" 1>&2
-      if ! /usr/bin/aws s3 cp --quiet ${DBDIR}/${DBNAME}.gz s3://${BL_ENVIRONMENT}/dbbackups/${TENANT}/${DBNAME}.gz
+      if ! /usr/bin/aws s3 cp --no-progress ${DBDIR}/${DBNAME}.gz s3://${BL_ENVIRONMENT}/dbbackups/${TENANT}/${DBNAME}.gz
       then
          echo "CLEANUP - $TENANT - $(/bin/date) - copy to s3 failed, aborting" 1>&2
          /usr/bin/mail -r "cspace-support@lists.berkeley.edu" -s "CLEANUP - $BL_ENVIRONMENT $TENANT Blacklight DB clean up failed" ${NOTIFY} <<< 'Unable to copy to S3, see dbclean.log for details.'
       else
-         ERRORS=0
+         SQLERRORS=0
 
-         cd /home/app_cspace/projects/search_$TENANT
          echo "CLEANUP - $TENANT - $(/bin/date) - deleting searches" 1>&2
-         if ! rake blacklight:delete_old_searches
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from searches;"
          then
-            ((ERRORS++))
+            ((SQLERRORS++))
          fi
 
-         echo "CLEANUP - $TENANT - $(/bin/date) - deleting guests" 1>&2
-         if ! rake devise_guests:delete_old_guest_users
+         echo "CLEANUP - $TENANT - $(/bin/date) - deleting users" 1>&2
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from users where email like 'guest_%';"
          then
-            ((ERRORS++))
+            ((SQLERRORS++))
          fi
 
-         # for now, let's skip cleaning up bookmarks: there are so few of them
-         # echo "CLEANUP - $TENANT - $(/bin/date) - deleting bookmarks" 1>&2
-         # if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from bookmarks where user_id not in (select id from users);"
-         # then
-         #    ((ERRORS++))
-         # fi
+         echo "CLEANUP - $TENANT - $(/bin/date) - deleting bookmarks" 1>&2
+         if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "delete from bookmarks where user_id not in (select id from users);"
+         then
+            ((SQLERRORS++))
+         fi
 
          echo "CLEANUP - $TENANT - $(/bin/date) - vacuuming" 1>&2
          if ! /usr/bin/sqlite3 ${DBDIR}/production.sqlite3 "vacuum;"
          then
-            ((ERRORS++))
+            ((SQLERRORS++))
          fi
 
-         if [ "$ERRORS" -gt "0" ]
+         if [ "$SQLERRORS" -gt "0" ]
          then
             /usr/bin/mail -r "cspace-support@lists.berkeley.edu" -s "CLEANUP - $BL_ENVIRONMENT $TENANT Blacklight DB clean up completed with errors" ${NOTIFY} <<< 'See dbclean.log for details.'
          fi
       fi
    fi
 
-   if [ "$ERRORS" -eq "0" ]
+   if [ "$SQLERRORS" -eq "0" ]
    then
       echo "CLEANUP - $TENANT - $(/bin/date) - removing 4 week old backup" 1>&2
       /bin/rm -f ${DBDIR}/production.sqlite3.${TENANT}."$(/bin/date +%Y%m%d --date='4 weeks ago')".gz
