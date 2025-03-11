35f77b0a7872561602edbf61628ccc768ffa9e75
Anne Lyttle <alyttle@berkeley.edu>
2025-03-11 09:52:47 -0700

    HMP-365: adds/improves accessible names + misc SR fixes

--- portal/app/helpers/url_helper.rb
+++ portal/app/helpers/url_helper.rb
@@ -2,9 +2,13 @@ module UrlHelper
   include Blacklight::UrlHelperBehavior
 
   # Search History and Saved Searches display
-  def link_to_previous_search(params)
+  def link_to_previous_search(params, index, count)
     Deprecation.silence(SearchHistoryConstraintsHelper) do
-      link_to(render_search(params), search_action_path(params))
+      link_to(
+        render_search(params),
+        search_action_path(params),
+        aria: { label: "recent search #{index + 1} of #{count}" }
+      )
     end
   end
 end
