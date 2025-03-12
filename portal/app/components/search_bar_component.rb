fb2844c6d1efee5766d925318d759a6f205ebdba
Anne Lyttle <alyttle@berkeley.edu>
2025-03-12 13:53:22 -0700

    HMP-372: fixes operator label, duplicate IDs

--- portal/app/components/search_bar_component.rb
+++ portal/app/components/search_bar_component.rb
@@ -2,4 +2,23 @@
 
 class SearchBarComponent < Blacklight::SearchBarComponent
 
+  def initialize(
+    url:,
+    params:,
+    advanced_search_url: nil,
+    presenter: nil,
+    classes: ['search-query-form'],
+    prefix: nil,
+    method: 'GET',
+    q: nil,
+    query_param: :q,
+    search_field: nil,
+    search_fields: nil,
+    autocomplete_path: nil,
+    autofocus: nil,
+    i18n: { scope: 'blacklight.search.form' },
+    form_options: {}
+  )
+    return super(url: url, params: params, prefix: 'search-bar-')
+  end
 end
