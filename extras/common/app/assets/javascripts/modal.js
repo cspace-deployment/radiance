dd6e29ae9a14d3e3e218dcdec998e3326a7a3dd8
Anne Lyttle <alyttle@berkeley.edu>
2025-05-08 13:39:31 -0700

    HMP-390: traps focus in modal dialogs

--- extras/common/app/assets/javascripts/modal.js
+++ extras/common/app/assets/javascripts/modal.js
@@ -1,41 +1,134 @@
-const focusableDescendantsSelector = ':where(button, input:not([type="hidden"]), textarea, select, a:any-link, *[tabindex]):not([aria-hidden="true"]):not([hidden]):not(disabled'
+/**
+ * modal.js
+ */
+
+class FocusTrap {
+  constructor(modalEl) {
+    this.modalEl = modalEl
+  }
+  activate = (infix = '') => {
+    this.#addFocusGuard(`focus-trap-${infix}begin`, this.focusLastDescendant)
+    this.#addFocusGuard(`focus-trap-${infix}end`, this.focusFirstDescendant)
+  }
+  deactivate = (infix = '') => {
+    this.#removeFocusGuard(`focus-trap-${infix}begin`, this.focusLastDescendant)
+    this.#removeFocusGuard(`focus-trap-${infix}end`, this.focusFirstDescendant)
+  }
+  focusFirstDescendant = e => {
+    const firstFocusable = this.modalEl.querySelector(focusableDescendantsSelector)
+    if (e) {
+      e.preventDefault()
+    }
+    putFocus(firstFocusable)
+  }
+  focusLastDescendant = e => {
+    const focusableDescendants = this.modalEl.querySelectorAll(focusableDescendantsSelector)
+    if (e) {
+      e.preventDefault()
+    }
+    putFocus(focusableDescendants[focusableDescendants.length - 1])
+  }
+  // private
+  #addFocusGuard = (id, onFocus) => {
+    const guardEl = document.getElementById(id)
+    if (guardEl) {
+      guardEl.setAttribute('tabindex', 0)
+      guardEl.addEventListener('focus', onFocus)
+    }
+  }
+  #removeFocusGuard = (id, onFocus) => {
+    const guardEl = document.getElementById(id)
+    if (guardEl) {
+      guardEl.removeAttribute('tabindex', 0)
+      guardEl.removeEventListener('focus', onFocus)
+    }
+  }
+}
+
+const focusableDescendantsSelector = ':where(button, input:not([type="hidden"]), textarea, select, a:any-link, *[tabindex]):not([aria-hidden="true"]):not([hidden]):not(disabled):not([id^="focus-trap"])'
 
 const isValidTabindex = v => !Number.isNaN(Number.parseInt(v, 10))
+
+const putFocus = el => {
+  if (el) {
+    let counter = 0
+    const attemptFocus = setInterval(() => {
+      el.focus()
+      if (el === document.activeElement || ++counter > 4) {
+        // Abort after success or five attempts
+        clearInterval(attemptFocus)
+      }
+    }, 100)
+  }
+}
+
+const cacheAriaHidden = el => {
+  return new Promise((resolve) => {
+    if (el.hasAttribute('aria-hidden')) {
+      el.dataset.ariaHidden = el.getAttribute('aria-hidden')
+    }
+    resolve()
+  })
+}
+
+const restoreAriaHidden = el => {
+  new Promise((resolve) => {
+    if (el.dataset.ariaHidden) {
+      el.setAttribute('aria-hidden', el.dataset.ariaHidden)
+    } else {
+      el.removeAttribute('aria-hidden')
+    }
+    resolve()
+  }).then(() => delete el.dataset.ariaHidden)
+}
+
+const cacheTabIndex = el => {
+  return new Promise((resolve) => {
+    if (isValidTabindex(el.tabindex)) {
+      el.dataset.tabindex = el.tabindex
+    }
+    resolve()
+  })
+}
+
+const restoreTabIndex = el => {
+  new Promise((resolve) => {
+    if (isValidTabindex(el.dataset.tabindex)) {
+      el.setAttribute('tabindex', el.dataset.tabindex)
+    } else {
+      el.removeAttribute('tabindex')
+    }
+    resolve()
+  }).then(() => delete el.dataset.tabindex)
+}
 
 const toggleBackgroundElementsDisabled = (disable, modalId) => {
   // The modal element is a child of <body>. When the modal opens we need to disable its siblings.
   // Any of these siblings' descendants that can receive focus must also be disabled.
-  // If a descendant has a tabindex, capture it in order to restore the element to its original state upon re-enabling.
   const bodyEl = $('body')[0]
   const backgroundElements = Array.from(bodyEl.children)
   backgroundElements.forEach(el => {
-    if (['NAV', 'DIV', 'MAIN', 'FOOTER'].includes(el.tagName.toUpperCase()) && modalId !== el.id) {
+    if (['NAV', 'DIV', 'MAIN', 'FOOTER'].includes(el.tagName.toUpperCase()) && ![modalId || 'blacklight-modal'].includes(el.id)) {
       if (disable) {
         const focusableDescendants = Array.from(el.querySelectorAll(focusableDescendantsSelector))
         el.setAttribute('inert', true)
-        el.setAttribute('aria-hidden', true)
+        cacheAriaHidden(el).then(() => el.setAttribute('aria-hidden', true))
+
         focusableDescendants.forEach(child => {
           child.setAttribute('aria-disabled', true)
           child.setAttribute('disabled', true)
+          cacheTabIndex(child).then(() => child.setAttribute('tabindex', -1))
           child.dataset.focusableHidden = true
-          if (isValidTabindex(child.tabindex)) {
-            child.dataset.tabindex = child.tabindex
-          }
-          child.setAttribute('tabindex', -1)
         })
       } else {
         const focusableDescendants = Array.from(el.querySelectorAll('[data-focusable-hidden]'))
         el.removeAttribute('inert')
-        el.removeAttribute('aria-hidden')
+        restoreAriaHidden(el)
+
         focusableDescendants.forEach(child => {
           child.removeAttribute('aria-disabled')
           child.removeAttribute('disabled')
-          if (isValidTabindex(child.dataset.tabindex)) {
-            child.setAttribute('tabindex', child.dataset.tabindex)
-            delete child.dataset.tabindex
-          } else {
-            child.removeAttribute('tabindex')
-          }
+          restoreTabIndex(child)
           delete child.dataset.focusableHidden
         })
       }
@@ -43,20 +136,19 @@ const toggleBackgroundElementsDisabled = (disable, modalId) => {
   })
 }
 
-const disableContentBehindModal = e => {
+const onModalShown = e => {
   // When the modal is open, content outside the modal should not be accessible via keyboard or assistive technology.
   const modalEl = e.target
   const modalTitle = modalEl.querySelector('#modal-title').textContent
-  setTimeout(() => {
-    const firstFocusableChild = modalEl.querySelector(focusableDescendantsSelector) || modalEl.querySelector('#blacklight-modal-close')
-    if (firstFocusableChild) {
-      firstFocusableChild.focus()
-      toggleBackgroundElementsDisabled(true, 'blacklight-modal')
-    }
-  }, 400)
-  modalEl.setAttribute('aria-hidden', 'false')
+  const focusTrap = new FocusTrap(modalEl)
+
+  focusTrap.focusFirstDescendant()
+  focusTrap.activate()
+  toggleBackgroundElementsDisabled(true)
+
   $(modalEl).on('hide.bs.modal', () => {
     onModalWillHide(modalTitle)
+    focusTrap.deactivate()
     $(modalEl).off('shown.bs.modal')
   })
 }
@@ -66,14 +158,15 @@ const onModalWillHide = modalTitle => {
   // Return focus to the element that triggered the modal.
   const selector = `:contains("${modalTitle}")`
   const modalTrigger = $(`${Blacklight.modal.triggerLinkSelector}${selector}, ${Blacklight.modal.triggerFormSelector}${selector}`)
-  toggleBackgroundElementsDisabled(false, 'blacklight-modal')
-  modalTrigger[0].focus()
+  toggleBackgroundElementsDisabled(false)
+  putFocus(modalTrigger[0])
 }
 
-const onModalShown = e => {
-  $(e.target).on('shown.bs.modal', disableContentBehindModal)
+const onModalInitialized = e => {
+  e.target.removeAttribute('aria-hidden')
+  $(e.target).on('shown.bs.modal', onModalShown)
 }
 
 window.addEventListener('load', () => {
-  $('body').on('loaded.blacklight.blacklight-modal', onModalShown)
+  $('body').on('loaded.blacklight.blacklight-modal', onModalInitialized)
 })
